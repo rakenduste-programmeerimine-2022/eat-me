dbe916cb31713d849a75280aff965d5ae227cd47
Andres <andres.sikka98@gmail.com>
2022-11-20 23:17:00 +0200

    Backend and frontend upload

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../images/LogoBurger.svg'
+import Button from '@material-ui/core/Button'
+import {AppBar, Container, Toolbar, IconButton, Typography, Box} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles'
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  logoButton: {
+    marginRight: theme.spacing(1)
+  },
+  links: {
+    flexGrow: 1,
+    display: 'flex',
+    
+  }
+}))
+
+
+export const Header = () => {
+  const classes = useStyles();
+  
+  return (
+    <AppBar position="fixed" color="inherit">
+      <Container fixed>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" className={classes.logoButton}>
+            <Link to="/" ><img src={logo} height={80} alt='logo' /></Link>
+          </IconButton>
+          <Box className={classes.links}>
+            <Typography style={{marginRight:'30px', marginLeft:'30px'}} variant="h6">
+              <Link to="/recipes" style={{textDecoration: 'none', color: 'black'}}>Recipes</Link>
+            </Typography>
+            <Typography variant="h6">
+              <Link to="/about" style={{textDecoration: 'none', color: 'black'}}>About</Link>
+            </Typography>
+          </Box>
+          <Box mr={3}>
+            <Link to="/login" style={{textDecoration: 'none'}}>
+              <Button                
+                variant = 'contained'
+                style = {{color: 'black', backgroundColor: 'orange', borderColor: 'black', marginRight: '20px', width: '100px'}}                
+              >
+                Login
+              </Button>
+            </Link>
+            <Link to="/register" style={{textDecoration: 'none'}}>
+             <Button
+                variant = 'contained'
+                style = {{color: 'black', backgroundColor: 'orange', borderColor: 'black', marginRight: '20px', width: '100px'}}
+              >
+                Sign up
+              </Button>
+            </Link>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
